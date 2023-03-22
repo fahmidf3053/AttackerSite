608663ed31678d1c4d9574e9e8136468ee0ae09e
Fahmid Ahmed <fahmidahmed16@gmail.com>
2023-03-22 11:42:50 +0600

    initial commit

---
+++ AttackerSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AttackerSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
